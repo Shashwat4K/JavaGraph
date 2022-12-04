91de1f8b05c57ed423b830566425b7bfdd2c7a16
Shashwat Kadam <shashwatkdm@gmail.com>
2022-12-04 12:08:16 -0800

    Created menu. Users can select the graphs.

--- graph/Main.java
+++ graph/Main.java
@@ -3,6 +3,7 @@ package graph;
 import graph.util.GraphCreator;
 
 import java.util.Set;
+import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -14,6 +15,14 @@ import static graph.GraphOps.detectArticulationPoints_BruteForce;
  * Main class to test and debug the graph operations.
  */
 public class Main {
+    private static void menu() {
+        System.out.println("1) power-494-bus");
+        System.out.println("2) power-662-bus");
+        System.out.println("3) power-685-bus");
+        System.out.println("4) power-1138-bus");
+        System.out.println("5) power-bcspwr09");
+        System.out.println("0) Exit");
+    }
     private static void writeData(String filePath, String fileName, MyInteger efficientAlgorithmTotalTime, MyInteger bruteForceTotalTime) {
         try(
             FileWriter fr = new FileWriter(filePath + fileName + ".txt");
@@ -31,32 +40,60 @@ public class Main {
     }
     public static void main(String[] args) {
         GraphCreator g = new GraphCreator();
+        Scanner sc = new Scanner(System.in);
         try {
-            String fileName = "power-bcspwr09.mtx";
-            Graph<Node> myGraph = g.createGraph("data/power-bcspwr09/" + fileName, true);
-            System.out.println(fileName + ": V = " + myGraph.getVertexCount() + " E = " + myGraph.getEdgesCount());
-            MyInteger efficientAlgorithmTotalTime = new MyInteger(0);
-            MyInteger bruteForceTotalTime = new MyInteger(0);
-            Set<Node> articulationPoints = detectArticulationPoints(myGraph, false, efficientAlgorithmTotalTime);
-            Set<Node> articulationPointsBruteForce = detectArticulationPoints_BruteForce(myGraph, false, bruteForceTotalTime);
-            // Set<Node> apUSGraph = detectArticulationPoints(usGraph, false, new MyInteger(0));
-            for(Node node: articulationPoints) { 
-                if (!articulationPointsBruteForce.contains(node)) {
-                    System.out.println("Node " + node.getValue() + " is not in both sets!");
+            menu();
+            String graphFile = "";
+            int userInput = sc.nextInt();
+            switch (userInput) {
+                case 1:
+                    graphFile = "power-494-bus";
                     break;
-                }
-                System.out.print("Number of CCs after disconnecting AP " + node.getLabel() + " are: ");
-                myGraph.disableVertex(node);
-                int cc = countConnectedComponents(myGraph);
-                System.out.println(cc);
-                myGraph.enableVertex(node);
+                case 2:
+                    graphFile = "power-662-bus";
+                    break;
+                case 3:
+                    graphFile = "power-685-bus";
+                    break;
+                case 4:
+                    graphFile = "power-1138-bus";
+                    break;
+                case 5:
+                    graphFile = "power-bcspwr09";
+                    break;
+                case 0:
+                default:
+                    System.out.println("Exitting...");
+                    break;
             }
-            writeData(
-                "data/demo_data/running_time_data/", // TODO: Change the path to an appropriate location in your device.
-                fileName, 
-                efficientAlgorithmTotalTime, 
-                bruteForceTotalTime
-            );
+            if (userInput > 0 && userInput <= 5) {
+                
+                String fileName = graphFile + ".mtx";
+                Graph<Node> myGraph = g.createGraph("src/data/" + graphFile + "/" + fileName, true);
+                System.out.println(fileName + ": V = " + myGraph.getVertexCount() + " E = " + myGraph.getEdgesCount());
+                MyInteger efficientAlgorithmTotalTime = new MyInteger(0);
+                MyInteger bruteForceTotalTime = new MyInteger(0);
+                Set<Node> articulationPoints = detectArticulationPoints(myGraph, false, efficientAlgorithmTotalTime);
+                Set<Node> articulationPointsBruteForce = detectArticulationPoints_BruteForce(myGraph, false, bruteForceTotalTime);
+                // Set<Node> apUSGraph = detectArticulationPoints(usGraph, false, new MyInteger(0));
+                for(Node node: articulationPoints) { 
+                    if (!articulationPointsBruteForce.contains(node)) {
+                        System.out.println("Node " + node.getValue() + " is not in both sets!");
+                        break;
+                    }
+                    System.out.print("Number of CCs after disconnecting AP " + node.getLabel() + " are: ");
+                    myGraph.disableVertex(node);
+                    int cc = countConnectedComponents(myGraph);
+                    System.out.println(cc);
+                    myGraph.enableVertex(node);
+                }
+                writeData(
+                    "src/data/demo_data/running_time_data/", // TODO: Change the path to an appropriate location in your device.
+                    fileName, 
+                    efficientAlgorithmTotalTime, 
+                    bruteForceTotalTime
+                );
+            }
         } catch(IOException e) {
             e.printStackTrace();
         }
